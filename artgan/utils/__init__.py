ef35b584bb513930ec933f0f8cdfe48695d0c1c7
Nils Schaetti <n.schaetti@gmail.com>
2022-06-07 21:14:24 +0200

    DCGAN, WGAN, Conditional GAN

--- artgan/utils/__init__.py
+++ artgan/utils/__init__.py
@@ -2,7 +2,13 @@
 
 # Imports
 from .vizu import show_tensor_images
+from .music import write_curve
+from .gradient import gradient_penalty, get_gradient, get_crit_loss, get_gen_loss
+from .utils import get_noise
 
 __all__ = [
-    "show_tensor_images"
+    "show_tensor_images",
+    "write_curve",
+    "gradient_penalty", "get_gradient", "get_crit_loss", "get_gen_loss",
+    'get_noise'
 ]
